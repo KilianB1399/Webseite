de0f3d769c301c0c5f73e23d4791a90b9f26f6b8
KilianB1399 <kilianbogus@bbd-8.org>
2024-08-24 21:16:20 +0200

    Add files via upload

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,20 +1,22 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+function submitForm() {
+    var form = document.getElementById("contact-form");
+    var name = document.getElementById("name").value;
+    var lastName = document.getElementById("last-name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var telephone = document.getElementById("telephone").value;
+    var message = document.getElementById("message").value;
 
-    // Create a new FormData object
-    const formData = new FormData(event.target);
+    var mailtoLink = "mailto:bogus.kilian@outlook.de"
+        + "?subject=" + encodeURIComponent(subject)
+        + "&body=" + encodeURIComponent("Name: " + name + "\n"
+            + "Nachname: " + lastName + "\n"
+            + "E-Mail: " + email + "\n"
+            + "Telefonnummer: " + telephone + "\n"
+            + "Nachricht: " + message);
 
-    // Send form data to the server using Fetch API
-    fetch('php/send_email.php', {  // Update the path to match the location of your PHP file
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text()) // Get response as text
-    .then(data => {
-        alert(data); // Show response message
-    })
-    .catch(error => {
-        console.error('Error:', error); // Log any errors to the console
-        alert('There was an error sending your message. Please try again later.');
-    });
-});
+    window.location.href = mailtoLink;
+
+    // Reset the form fields
+    form.reset();
+}
